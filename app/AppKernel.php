183fb6adac4f6529f4ee7028620fe14e7bcbb6cf
Unknown <quentiin-25@live.fr>
2017-09-06 23:26:19 +0200

    Ajout de bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new ContratBundle\ContratBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/ContratBundle/Form/ContratType.php
+++ src/ContratBundle/Form/ContratType.php
@@ -5,6 +5,7 @@ namespace ContratBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ContratType extends AbstractType
 {
@@ -13,7 +14,31 @@ class ContratType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nomNaissance')->add('nomUsage')->add('prenom')->add('adresse')->add('ville')->add('enQualite')->add('tel')->add('numero')->add('nomEnfant')->add('prenomEnfant')->add('dateNaissance')->add('dateEffetContrat')->add('dureePeriodeEssai')->add('nbHeures')->add('jourRepos')->add('isMensuel')->add('nbSemaines')->add('salaireBrut')->add('salaireNet')->add('salaireMenseulleNet')->add('modaliteCongePayes')->add('prixIndemnite')->add('repasFournis');
+        $builder
+            ->add('nomNaissance')
+            ->add('nomUsage')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('ville')
+            ->add('enQualite')
+            ->add('tel')
+            ->add('numero')
+            ->add('nomEnfant')
+            ->add('prenomEnfant')
+            ->add('dateNaissance')
+            ->add('dateEffetContrat')
+            ->add('dureePeriodeEssai')
+            ->add('nbHeures')
+            ->add('jourRepos')
+            ->add('isMensuel')
+            ->add('nbSemaines')
+            ->add('salaireBrut')
+            ->add('salaireNet')
+            ->add('salaireMenseulleNet')
+            ->add('modaliteCongePayes')
+            ->add('prixIndemnite')
+            ->add('repasFournis')
+            ->add('save', SubmitType::class, array('label' => 'Ajout d\'un contrat'));
     }
     
     /**
